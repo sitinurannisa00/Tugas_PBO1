828f14f27407400450e9644a6044da34984015cf
sitinurannisa000 <stnurannisa000@gmail.com>
2021-06-23 08:38:17 +0300

    membuat 3 atribut dan 2 method

--- src/model/AksiDaelerMobil.java
+++ src/model/AksiDaelerMobil.java
@@ -1,4 +1,23 @@
 package model;
 
 public class AksiDaelerMobil {
+    public static void main (String [] args){
+
+        //object class 1
+        DaelerMobil  DaelerMobil = new DaelerMobil();
+        DaelerMobil.NamaMobil = "Honda Brio";
+        DaelerMobil.TypeMobil = "Honda";
+        DaelerMobil.WarnaMobil = "Merah";
+        DaelerMobil.TahunKeluarMobil= "2021";
+        DaelerMobil.NomerMesinMobil=" 345678956245";
+        DaelerMobil.NomerKerangkaMobil ="243567865689";
+        DaelerMobil.ModelMobil="Brio E CVT";
+        DaelerMobil.HargaMobil= 175000000;
+
+        //Object class 2
+        PembeliMobil pembeli = new PembeliMobil();
+        pembeli.NamaMobil = "Honda ";
+
+
+    }
 }
--- src/model/PembeliMobil.java
+++ src/model/PembeliMobil.java
@@ -1,4 +1,23 @@
 package model;
 
-public class PembeliMobil {
+public class PembeliMobil extends DaelerMobil {
+
+    public String NamaPembeli;
+    public int NamaPenjual;
+    public String Jumlahbeli;
+
+    public void infopenjual(){
+        System.out.println("==================");
+        System.out.println("Nama Pembeli Mobil : "+NamaPembeli);
+        System.out.println("Nama Penjual Mobil : "+NamaPenjual);
+        System.out.println(" Jumlah Beli       : "+Jumlahbeli);
+    }
+    public void jumlahinfo(){
+        String ji = "INSERT INTO DaelerMobil VALUES ('"+Jumlahbeli+"')";
+        System.out.println(ji);
+
+    }
+    //Getter And Sister
+
+
 }
