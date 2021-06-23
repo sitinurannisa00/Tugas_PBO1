e4029461f0c67128a3f16bb7637243aaa1ce326b
sitinurannisa000 <stnurannisa000@gmail.com>
2021-06-23 07:49:25 +0300

    memebuat 8atribut dan 4 method

--- src/model/DaelerMobil.java
+++ src/model/DaelerMobil.java
@@ -1,4 +1,52 @@
 package model;
 
 public class DaelerMobil {
+//    8 atribut clas DaelerMobil
+
+    public String NamaMobil;
+    public String TypeMobil;
+    public String WarnaMobil;
+    public String TahunKeluarMobil;
+    public String NomerMesinMobil;
+    public String NomerKerangkaMobil;
+    public String ModelMobil;
+    public int HargaMobil;
+
+    public void tampilkanAtribut() {
+        System.out.println("==========================");
+        System.out.println("Nama Mobil : " + NamaMobil);
+        System.out.println("TypeMobil  : " + TypeMobil);
+        System.out.println("Warna Mobil : " + WarnaMobil);
+        System.out.println("Tahun Keluar Mobil : " + TahunKeluarMobil);
+        System.out.println("NomerMesinMobil : " + NomerMesinMobil);
+        System.out.println("NomerKerangkaMobil : " + NomerKerangkaMobil);
+        System.out.println("ModelMobil : " + ModelMobil);
+        System.out.println("HergaMobil : " + HargaMobil);
+    }
+
+    public void tampilkaninfoDealerMobil() {
+        System.out.println("===========================");
+        System.out.println("Nama Mobil : " + NamaMobil);
+        System.out.println("Type Mobil: " + TypeMobil);
+        System.out.println("WarnaMobil : " + WarnaMobil);
+        System.out.println("Tahun Keluar Mobil : " + TahunKeluarMobil);
+        System.out.println("ModelMobil :" + ModelMobil);
+    }
+
+    public boolean create() {
+        String insertCreate = "INSERT INTO DealerMobil VALUES ('" + NamaMobil + "','" + TypeMobil + "','" + ModelMobil + "','" + WarnaMobil + "')";
+        System.out.println(insertCreate);
+        boolean berhasil = true;
+        if (berhasil) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean create(String TypeMobil) {
+        String insertCreate = "INSERT INTO DaelerMobil VALUES Type Mobil " + TypeMobil;
+        System.out.println(insertCreate);
+        return false;
+    }
 }
