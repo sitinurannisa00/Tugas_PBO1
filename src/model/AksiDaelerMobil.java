4ea6a952ae2a9737a57a2c27083f26800117407d
sitinurannisa000 <stnurannisa000@gmail.com>
2021-06-23 09:59:46 +0300

    Membuat Getter and Setter di clas 1

--- src/model/AksiDaelerMobil.java
+++ src/model/AksiDaelerMobil.java
@@ -16,19 +16,36 @@ public class AksiDaelerMobil {
 
         //Object class 2
         PembeliMobil pembeli = new PembeliMobil();
-        pembeli.NamaMobil = "Honda Brio";
+        pembeli.NamaMobil = "jazz";
         pembeli.TypeMobil = "Honda";
         pembeli.WarnaMobil= "Merah";
         pembeli.TahunKeluarMobil = "2021";
-        pembeli.NomerMesinMobil ="345678956245";
-        pembeli.NomerKerangkaMobil ="243567865689";
-        pembeli.ModelMobil="Brio E CVT";
+        pembeli.NomerMesinMobil ="xx345678956245";
+        pembeli.NomerKerangkaMobil ="xaxx243567865689";
+        pembeli.ModelMobil="Honda E MT";
         pembeli.HargaMobil= 175000000;
         pembeli.tampilkanAtribut();
         pembeli.create();
-        pembeli.create("Honda Brio");
+        pembeli.create("Honda Jazz");
 
         //construktor dengan mothod (obeject)
+        DaelerMobil Mobil1 = new DaelerMobil();
+        Mobil1.tampilkanAtribut();
+        DaelerMobil Mobil2 = new DaelerMobil();
+        Mobil2.tampilkaninfoDealerMobil();
+
+        DaelerMobil Mobil3 = new DaelerMobil("inova", "hitam");
+        Mobil3.tampilkaninfoDealerMobil();
+        DaelerMobil Mobil3 = new DaelerMobil("yaris", "merah");
+        Mobil3.tampilkanAtribut();
+
+
+        DaelerMobil Mobil5 = new DaelerMobil("HONDA","Honda E MT","2021");
+        Mobil3.tampilkaninfoDealerMobil();
+        DaelerMobil Mobil6 = new DaelerMobil("Toyota","Honda E MT","2021");
+        Mobil3.tampilkanAtribut();
+
+
 
 
 
--- src/model/DaelerMobil.java
+++ src/model/DaelerMobil.java
@@ -17,7 +17,7 @@ public class DaelerMobil {
         System.out.println("Nama Mobil : " + NamaMobil);
         System.out.println("TypeMobil  : " + TypeMobil);
         System.out.println("Warna Mobil : " + WarnaMobil);
-        System.out.println("Tahun Keluar Mobil : " + TahunKeluarMobil);
+        System.out.println("Tahun Keluar Mobil : " +TahunKeluarMobil);
         System.out.println("NomerMesinMobil : " + NomerMesinMobil);
         System.out.println("NomerKerangkaMobil : " + NomerKerangkaMobil);
         System.out.println("ModelMobil : " + ModelMobil);
@@ -26,15 +26,15 @@ public class DaelerMobil {
 
     public void tampilkaninfoDealerMobil() {
         System.out.println("===========================");
-        System.out.println("Nama Mobil : " + NamaMobil);
-        System.out.println("Type Mobil: " + TypeMobil);
-        System.out.println("WarnaMobil : " + WarnaMobil);
-        System.out.println("Tahun Keluar Mobil : " + TahunKeluarMobil);
-        System.out.println("ModelMobil :" + ModelMobil);
+        System.out.println("Nama Mobil : " +NamaMobil);
+        System.out.println("Type Mobil: " +TypeMobil);
+        System.out.println("WarnaMobil : " +WarnaMobil);
+        System.out.println("Tahun Keluar Mobil : " +TahunKeluarMobil);
+        System.out.println("ModelMobil :" +ModelMobil);
     }
 
     public boolean create() {
-        String insertCreate = "INSERT INTO DealerMobil VALUES ('" + NamaMobil + "','" + TypeMobil + "','" + ModelMobil + "','" + WarnaMobil + "')";
+        String insertCreate = "INSERT INTO DaelerMobil VALUES ('"+NamaMobil+"','"+TypeMobil+"','"+ModelMobil+"','"+WarnaMobil+"')";
         System.out.println(insertCreate);
         boolean berhasil = true;
         if (berhasil) {
@@ -45,8 +45,92 @@ public class DaelerMobil {
     }
 
     public boolean create(String TypeMobil) {
-        String insertCreate = "INSERT INTO DaelerMobil VALUES Type Mobil " + TypeMobil;
+        String insertCreate = "INSERT INTO DaelerMobil VALUES Type Mobil "+TypeMobil;
         System.out.println(insertCreate);
         return false;
+    }
+
+    //construktor
+    public DaelerMobil(){
+        System.out.println("===================");
+        NamaMobil = "please isi Nama Mobil";
+        TypeMobil = "please isi Type Mobil";
+        WarnaMobil = "please isi Warna Mobil";
+        ModelMobil = "please isi Model Mobil";
+    }
+    public DaelerMobil(String NamaMobil, String WarnaMobil){
+        this.NamaMobil = NamaMobil;
+        this.WarnaMobil = WarnaMobil ;
+    }
+    public DaelerMobil(String TypeMobil, String ModelMobil, String TahunKeluarMobil){
+        this.TypeMobil = TypeMobil;
+        this.ModelMobil = ModelMobil;
+        this.TahunKeluarMobil = TahunKeluarMobil;
+    }
+
+    //Getter and setter
+
+    public String getNamaMobil() {
+        return NamaMobil;
+    }
+
+    public void setNamaMobil(String namaMobil) {
+        NamaMobil = namaMobil;
+    }
+
+    public String getTypeMobil() {
+        return TypeMobil;
+    }
+
+    public void setTypeMobil(String typeMobil) {
+        TypeMobil = typeMobil;
+    }
+
+    public String getWarnaMobil() {
+        return WarnaMobil;
+    }
+
+    public void setWarnaMobil(String warnaMobil) {
+        WarnaMobil = warnaMobil;
+    }
+
+    public String getTahunKeluarMobil() {
+        return TahunKeluarMobil;
+    }
+
+    public void setTahunKeluarMobil(String tahunKeluarMobil) {
+        TahunKeluarMobil = tahunKeluarMobil;
+    }
+
+    public String getNomerMesinMobil() {
+        return NomerMesinMobil;
+    }
+
+    public void setNomerMesinMobil(String nomerMesinMobil) {
+        NomerMesinMobil = nomerMesinMobil;
+    }
+
+    public String getNomerKerangkaMobil() {
+        return NomerKerangkaMobil;
+    }
+
+    public void setNomerKerangkaMobil(String nomerKerangkaMobil) {
+        NomerKerangkaMobil = nomerKerangkaMobil;
+    }
+
+    public String getModelMobil() {
+        return ModelMobil;
+    }
+
+    public void setModelMobil(String modelMobil) {
+        ModelMobil = modelMobil;
+    }
+
+    public int getHargaMobil() {
+        return HargaMobil;
+    }
+
+    public void setHargaMobil(int hargaMobil) {
+        HargaMobil = hargaMobil;
     }
 }
