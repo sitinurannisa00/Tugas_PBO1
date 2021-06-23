d6a4b8c06997516b6d39b0b3a3ed9615f8bc2a33
sitinurannisa000 <stnurannisa000@gmail.com>
2021-06-23 10:01:37 +0300

    Membuat Getter and Setter di clas 2

--- src/model/PembeliMobil.java
+++ src/model/PembeliMobil.java
@@ -17,7 +17,30 @@ public class PembeliMobil extends DaelerMobil {
         System.out.println(ji);
 
     }
-    //Getter And Sister
+    //Getter And Setter
 
 
+    public String getNamaPembeli() {
+        return NamaPembeli;
+    }
+
+    public void setNamaPembeli(String namaPembeli) {
+        NamaPembeli = namaPembeli;
+    }
+
+    public int getNamaPenjual() {
+        return NamaPenjual;
+    }
+
+    public void setNamaPenjual(int namaPenjual) {
+        NamaPenjual = namaPenjual;
+    }
+
+    public String getJumlahbeli() {
+        return Jumlahbeli;
+    }
+
+    public void setJumlahbeli(String jumlahbeli) {
+        Jumlahbeli = jumlahbeli;
+    }
 }
